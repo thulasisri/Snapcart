ace17e7d1753f6e14923d40953953b85048bf93b
thulasisri <thulasisribalamurugan@gmail.com>
2025-07-12 12:56:17 +0530

    first commit

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "../context/CartContext";
- import './productcard.css'; 
+import './ProductCard.css'; // ✅ exact match
 
 const ProductCard = ({ product }) => {
   const { addToCart } = useCart();
